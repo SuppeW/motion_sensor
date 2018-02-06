bdbaf6d892cf44d501306f21cb33e58f7d965202
SuppeW <anders.woien96@gmail.com>
2018-02-06 14:18:35 +0100

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -5,5 +5,5 @@ while stop == False:
     rn = str(datetime.datetime.now().time())
     print("your alarm is set to go off at 14:20")
     print(rn)
-    if rn >= "14:20:00.000000":
+    if rn >= "14:19:00.000000":
         stop = True
