af41265b136e139a86fe0313e0138482c28155c8
SuppeW <anders.woien96@gmail.com>
2018-02-12 22:30:23 +0100

    Added a clock for testing on the pi

---
+++ klokke.py
@@ -0,0 +1,28 @@
+import time
+
+
+try:
+  #Python 2
+  import Tkinter a tk
+  from Tkinter import *
+except ImportError:
+  #Python
+  import tkinter as tk
+  from tkinger import *
+
+
+def tick(time1''):
+  time2 = time.strfime('%I:%M:%S')
+  if time2 != time1:
+  time! = time2
+  clock_frame.config(text=time2)
+  clock_frame.after(200, tick)
+
+
+root = tk=TK()
+root.title('Digital Clock')
+clock_frame = tk.Label(rot, font=('times', 100, 'bold'), bg='black', fg='green')
+clock_frame.pack(fill'both', expand=1)
+root geometry('700x400')
+tick()
+root.mainloop()
